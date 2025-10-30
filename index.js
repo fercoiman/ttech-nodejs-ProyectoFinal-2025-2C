7d4e9e58b1f655f9cfa77a47e05483635732cd1b
fercoiman <fcoiman@frba.utn.edu.ar>
2025-10-30 20:39:41 -0300

    agrego algunos ejemplos de rutas

--- index.js
+++ index.js
@@ -1,12 +1,34 @@
-import http from "http";
+import express from "express";
+
+const app = express();
 const LOCAL_PORT = 3000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader("Content-type", "HTTP");
-  res.end("<h1> Hola desde HTTP<h1/>");
+app.listen(LOCAL_PORT, () => {
+  console.log(`Servidor corriendo en http://localhost:${LOCAL_PORT}`);
 });
 
-server.listen(LOCAL_PORT, () => {
-  console.log(`Servidor corriendo en http://localhost:${LOCAL_PORT}`);
+app.get("/", (req, res) => {
+  res
+    .type("html")
+    .send(
+      "<h1>Ejercicio Practico</h1><br><h2 style=color:red>Esta el la segunda línea</h2>"
+    );
+
+  //res.send("Hola desde Express!");
+});
+
+app.get("/ping", (req, res) => {
+  res.send("/pong");
+});
+
+app.get("/productos", (req, res) => {
+  res.send("Bienvenido a la página de productos");
+});
+
+app.get("/productos/14", (req, res) => {
+  res.send("Estás viendo el producto Nro. 14.");
+});
+
+app.get("/productos/16", (req, res) => {
+  res.send("Estás viendo el producto Nro. 16.");
 });
