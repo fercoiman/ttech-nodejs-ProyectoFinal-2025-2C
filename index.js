279f3d55a235444bccd963d83ddf4982bd18ce25
fercoiman <fcoiman@frba.utn.edu.ar>
2025-11-06 19:57:10 -0300

    modifico index.js

--- index.js
+++ index.js
@@ -6,8 +6,17 @@ const app = express();
 const LOCAL_PORT = 3000;
 
 app.use(cors());
+app.use(express.json());
 app.use("/api/products", router);
-
+app.post("/products", (req, res) => {
+  const { name, price } = req.body;
+  const product = {
+    //id: products.length + 1,
+    id: Math.max(...products.map((p) => p.id)) + 1,
+    name,
+    price,
+  };
+});
 app.listen(LOCAL_PORT, () => {
-  console.log(`Servidor corriendo en http://localhost:${LOCAL_PORT}`);
+  console.log(`Servidor Express en http://localhost:${LOCAL_PORT}`);
 });
