ba1d2d1fa2d4d73ce56944226ce8a2915cbb77d8
fercoiman <fcoiman@frba.utn.edu.ar>
2025-12-11 09:12:34 -0300

    corrijo API key

--- src/models/firebase.js
+++ src/models/firebase.js
@@ -5,7 +5,7 @@ import { configDotenv } from "dotenv";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: 
+  apiKey: "AIzaSyAQ_u1kcOrngclYJLsc7n5RpTphLLRgMQY",
   authDomain: "entregafinal-8e4a9.firebaseapp.com",
   projectId: "entregafinal-8e4a9",
   storageBucket: "entregafinal-8e4a9.firebasestorage.app",
@@ -18,3 +18,25 @@ const app = initializeApp(firebaseConfig);
 
 // Initialize Firestore
 export const db = getFirestore(app);
+
+/*
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAQ_u1kcOrngclYJLsc7n5RpTphLLRgMQY",
+  authDomain: "entregafinal-8e4a9.firebaseapp.com",
+  projectId: "entregafinal-8e4a9",
+  storageBucket: "entregafinal-8e4a9.firebasestorage.app",
+  messagingSenderId: "584130145148",
+  appId: "1:584130145148:web:29f0cdd1daec04770d52f9"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+
+*/
